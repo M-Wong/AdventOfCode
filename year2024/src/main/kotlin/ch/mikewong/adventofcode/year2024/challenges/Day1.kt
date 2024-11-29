c616930ef7f4876316b7e89e61160fd5f4e467c0
Mike Wong <wong@ubique.ch>
2024-11-29 14:31:16 +0100

    Prepare 2024 module

---
+++ year2024/src/main/kotlin/ch/mikewong/adventofcode/year2024/challenges/Day1.kt
@@ -0,0 +1,15 @@
+package ch.mikewong.adventofcode.year2024.challenges
+
+import ch.mikewong.adventofcode.common.challenges.Day
+
+class Day1 : Day<Int, Int>(2024, 1, "") {
+
+	override fun partOne(): Int {
+		return 0
+	}
+
+	override fun partTwo(): Int {
+		return 0
+	}
+
+}
