0833a51427afcae352db7f2e86bf057eaed316a6
Mike Wong <wong@ubique.ch>
2024-12-07 09:27:24 +0100

    Massively improve day 7

--- year2024/src/main/kotlin/ch/mikewong/adventofcode/year2024/challenges/Day7.kt
+++ year2024/src/main/kotlin/ch/mikewong/adventofcode/year2024/challenges/Day7.kt
@@ -26,7 +26,47 @@ class Day7 : Day<Long, Long>(2024, 7, "Bridge Repair") {
 		}
 	}
 
+	/**
+	 * Improved implementation, that recursively checks if the [expectedResult] can be reached with the given operands.
+	 * This goes in reverse from the [expectedResult], checks if it can be reached by applying the last operand in the list, then calls itself again.
+	 * This algorithm runs part 2 in ~4ms on my machine
+	 */
 	private fun canBeSolved(expectedResult: Long, operands: List<Long>, withConcatenation: Boolean): Boolean {
+		if (operands.size == 1) {
+			return operands[0] == expectedResult
+		} else {
+			val lastOperand = operands.last()
+
+			// Check if addition can be used to reach the expected result
+			if (expectedResult > lastOperand) {
+				if (canBeSolved(expectedResult - lastOperand, operands.dropLast(1), withConcatenation)) return true
+			}
+
+			// Check if multiplication can be used to reach the expected result
+			if (expectedResult % lastOperand == 0L) {
+				if (canBeSolved(expectedResult / lastOperand, operands.dropLast(1), withConcatenation)) return true
+			}
+
+			if (withConcatenation) {
+				// Check if concatenation can be used to reach the expected result
+				val expectedString = expectedResult.toString()
+				val operandString = lastOperand.toString()
+
+				if (expectedString.endsWith(operandString) && expectedString != operandString) {
+					if (canBeSolved(expectedString.removeSuffix(operandString).toLong(), operands.dropLast(1), true)) return true
+				}
+			}
+
+			return false
+		}
+	}
+
+	/**
+	 * First implementation, that recursively checks if the [expectedResult] can be reached with the given operands.
+	 * This applies the operations to the first two numbers of the [operands] list and adds it to the beginning of the operands, then calls itself again.
+	 * This algorithm runs part 2 in ~650ms on my machine
+	 */
+	private fun canBeSolvedRecursively(expectedResult: Long, operands: List<Long>, withConcatenation: Boolean): Boolean {
 		val first = operands[0]
 		val second = operands[1]
 		val remaining = operands.drop(2)
@@ -42,8 +82,8 @@ class Day7 : Day<Long, Long>(2024, 7, "Bridge Repair") {
 			if (sum == expectedResult || product == expectedResult) return true
 		} else {
 			// Otherwise, recursively check the remaining operands with the sum or product
-			if (canBeSolved(expectedResult, listOf(sum) + remaining, withConcatenation)) return true
-			if (canBeSolved(expectedResult, listOf(product) + remaining, withConcatenation)) return true
+			if (canBeSolvedRecursively(expectedResult, listOf(sum) + remaining, withConcatenation)) return true
+			if (canBeSolvedRecursively(expectedResult, listOf(product) + remaining, withConcatenation)) return true
 		}
 
 		if (withConcatenation) {
@@ -53,7 +93,7 @@ class Day7 : Day<Long, Long>(2024, 7, "Bridge Repair") {
 				if (concatenated == expectedResult) return true
 			} else {
 				// Otherwise, recursively check the remaining operands with the concatenation
-				if (canBeSolved(expectedResult, listOf(concatenated) + remaining, true)) return true
+				if (canBeSolvedRecursively(expectedResult, listOf(concatenated) + remaining, true)) return true
 			}
 		}
 
